3077da8f07635682dac1c50243704fae735ef743
Anton Älgmyr <anton@algmyr.se>
2026-02-02 10:09:23 +0100

    Add filtering mechanism to test runner.

--- lua/vclib/testing.lua
+++ lua/vclib/testing.lua
@@ -42,6 +42,7 @@ end
 
 FAIL = color "bright red"
 PASS = color "bright green"
+NOTE = color "bright black"
 RESET = color "reset"
 
 ---@param text string
@@ -111,28 +112,38 @@ function M.dedent(s)
   return table.concat(lines, "\n") .. "\n"
 end
 
-local function _run_test_suite(suite_name, test_suite)
+local function _run_test_suite(
+  module_name,
+  suite_name,
+  test_suite,
+  should_run_test
+)
   local suite_start_time = vim.uv.hrtime()
   local suite_failed = 0
   local suite_total = 0
+  local suite_skipped = 0
   local test_cases = test_suite.test_cases
   local test_function = test_suite.test
   for case_name, case in pairs(test_cases) do
-    local full_test_name = string.format("%s__%s", suite_name, case_name)
-
-    local status, err = pcall(function()
-      test_function(case)
-    end)
-    suite_total = suite_total + 1
-    if not status then
-      suite_failed = suite_failed + 1
-      output(colorize("✗ FAIL", FAIL) .. " " .. full_test_name)
-      if err then
-        -- Massage errors into a more readable format.
-        err = err:gsub(":(%s)", ":\n", 1)
-        err = "  " .. err:gsub("\n", "\n  ")
-        output(err)
+    local full_test_name =
+      string.format("%s::%s__%s", module_name, suite_name, case_name)
+    if should_run_test(full_test_name) then
+      local status, err = pcall(function()
+        test_function(case)
+      end)
+      if not status then
+        suite_failed = suite_failed + 1
+        output(colorize("✗ FAIL", FAIL) .. " " .. full_test_name)
+        if err then
+          -- Massage errors into a more readable format.
+          err = err:gsub(":(%s)", ":\n", 1)
+          err = "  " .. err:gsub("\n", "\n  ")
+          output(err)
+        end
       end
+      suite_total = suite_total + 1
+    else
+      suite_skipped = suite_skipped + 1
     end
   end
 
@@ -143,28 +154,55 @@ local function _run_test_suite(suite_name, test_suite)
   local timing = string.format("(%.1fms)", duration_ms)
   if suite_failed == 0 then
     symbol = colorize("✓", PASS)
-    outcome = string.format("(%d passed)", suite_total)
+    outcome = string.format("(all %d passed)", suite_total)
   else
     symbol = colorize("✗", FAIL)
     outcome = string.format("(%d/%d failed)", suite_failed, suite_total)
   end
   output { symbol, suite_name, outcome, timing }
-  return suite_failed, suite_total
+  if suite_skipped > 0 then
+    output {
+      " ",
+      string.format(
+        colorize("%d tests were skipped due to filtering", NOTE),
+        suite_skipped
+      ),
+    }
+  end
+  return suite_failed, suite_total, suite_skipped
 end
 
-function M.run_tests(test_modules)
+function M.run_tests(test_modules, options)
+  options = options or {}
+  local function should_run_test(name)
+    if not options.filter then
+      return true
+    end
+
+    -- Very magic for convenience.
+    local pattern = "\\v" .. options.filter
+    local ok, regex = pcall(vim.regex, pattern)
+    if not ok then
+      error("Invalid filter regex: " .. options.filter)
+    end
+    return regex:match_str(name) ~= nil
+  end
+
   local start_time = vim.uv.hrtime()
   local failed = 0
   local total = 0
+  local skipped = 0
   for _, test_module_name in ipairs(test_modules) do
     local test_module = require(test_module_name)
     -- Slightly less verbose name for the test module, removing first component.
     local file_name = test_module_name:match "^[^.]+%.(.+)$" or test_module_name
     output(string.format("=== Running tests in %s ===", file_name))
     for suite_name, test_suite in pairs(test_module) do
-      local suite_failed, suite_total = _run_test_suite(suite_name, test_suite)
+      local suite_failed, suite_total, suite_skipped =
+        _run_test_suite(file_name, suite_name, test_suite, should_run_test)
       failed = failed + suite_failed
       total = total + suite_total
+      skipped = skipped + suite_skipped
     end
   end
 
@@ -173,10 +211,14 @@ function M.run_tests(test_modules)
   local timing = string.format("(%.1fms)", total_duration_ms)
   local msg
   if failed == 0 then
-    msg = colorize(string.format("All %d tests passed", total), PASS)
+    msg = colorize("All tests passed", PASS)
   else
     msg = colorize(string.format("%d/%d tests failed", failed, total), FAIL)
   end
+  if skipped > 0 then
+    msg = msg .. colorize(string.format(" (%d skipped)", skipped), NOTE)
+  end
+
   output { msg, timing }
   if failed > 0 then
     vim.cmd "cq"
